7ee63c5ff89693b3f592821a9a0ced217988788d
Stephanoslangate <nzoux01@gmail.com>
2023-03-08 18:36:16 +0100

    code n1

---
+++ Cacd/javaspbootcv/src/main/java/sn/groupeisi/m2gl/javaspbootcv/JavaspbootcvApplication.java
@@ -0,0 +1,13 @@
+package sn.groupeisi.m2gl.javaspbootcv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaspbootcvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaspbootcvApplication.class, args);
+	}
+
+}
---
+++ Cacd/javaspbootcv/src/test/java/sn/groupeisi/m2gl/javaspbootcv/JavaspbootcvApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.groupeisi.m2gl.javaspbootcv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaspbootcvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
